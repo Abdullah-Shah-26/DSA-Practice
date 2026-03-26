20143a2151ff0b0cdd6b9585ff562e830c95f61e
Abdullah Shah <syedmohammedabdullahshah@gmail.com>
2026-03-27 04:02:18 +0530

    Update

--- LINE SWEEP/Count_Days_Without_Meetings.cpp
+++ LINE SWEEP/Count_Days_Without_Meetings.cpp
@@ -9,6 +9,7 @@ static const int MOD = 1e9 + 7;
 
 inline void fastio() { ios::sync_with_stdio(false); cin.tie(nullptr); }
 
+//! Approach 1 - LineSweep
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
@@ -45,3 +46,38 @@ public:
     return freeDays;
     }
 };
+
+//! Approach 2 - Sorting + Merge Intervals
+//! TC = O(N Log N)
+//! SC = O(1)
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+         int n = arr.size();
+         vector<vector<int>>ans;
+         sort(arr.begin(), arr.end());
+         int curSt = arr[0][0];
+         int curEnd = arr[0][1];
+
+         for(int i = 1; i < n; i++){
+          int nextSt = arr[i][0];
+          int nextEnd = arr[i][1];
+
+          if(nextSt <= curEnd){
+            // OverLap
+            curEnd = max(curEnd, nextEnd);
+          }
+          else{
+            // No Overlap
+            ans.push_back({curSt, curEnd});
+            curSt = nextSt;
+            curEnd = nextEnd;
+          }
+         }
+
+    // If all intervals merged, then else didnt get executed
+    ans.push_back({curSt, curEnd});   
+
+    return ans;
+    }
+}; 
