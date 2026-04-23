0f99f510454322d17e7650444736ed2fa2f7a532
Abdullah Shah <syedmohammedabdullahshah@gmail.com>
2026-04-24 01:56:46 +0530

    Update

--- DP/House_Robber_2.cpp
+++ DP/House_Robber_2.cpp
@@ -4,21 +4,21 @@ public:
     int Robbery(vector<int> &nums)
     {
         int n = nums.size();
-    if(n == 1 )
-    return nums[0];
+        if(n == 1 )
+        return nums[0];
 
-    if(n == 2)
-    return max(nums[0], nums[1]);
+        if(n == 2)
+        return max(nums[0], nums[1]);
 
-    int curr, prev =  max(nums[0], nums[1]), prev2 = nums[0];
+        int curr, prev =  max(nums[0], nums[1]), prev2 = nums[0];
 
-    for(int i = 2; i <n ; i++)
-    {
-        curr = max(nums[i] + prev2, prev);
-        prev2 = prev; 
-        prev = curr;
-    }
-    return curr;
+        for(int i = 2; i <n ; i++)
+        {
+            curr = max(nums[i] + prev2, prev);
+            prev2 = prev; 
+            prev = curr;
+        }
+        return curr;
     }
 
 
