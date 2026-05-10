e15a59099d07917fb5effa0c51328468f83573c8
Abdullah Shah <syedmohammedabdullahshah@gmail.com>
2026-05-11 03:10:34 +0530

    Update

---
+++ GREEDY ALGORITHM/Min_Ops_To_Make_Median_Equal_To_K.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// TC = O(NLogN)
+// SC = O(1) 
+
+class Solution {
+  public:
+    long long minOperationsToMakeMedianK(vector<int>& nums, int k) {
+    int n = nums.size();
+    int m = n/2;
+    long long ops = 0;
+
+    sort(nums.begin(), nums.end());
+
+    if(nums[m] == k) return 0;
+
+    if(nums[m] > k){
+      for(int i = 0; i <= m; i++){
+        if(nums[i] > k){
+          ops += (nums[i] - k);
+        }
+      }
+    }
+    else{
+      for(int i = m; i < n; i++){
+        if(nums[i] < k){
+          ops += (k - nums[i]);
+        }
+      }
+    }
+
+    return ops;
+    }
+};
