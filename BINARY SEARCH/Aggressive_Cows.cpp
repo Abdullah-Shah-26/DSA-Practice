dd3b832fcb7d5cca39a962d0d364f0879f76c845
Abdullah Shah <syedmohammedabdullahshah@gmail.com>
2026-05-17 20:25:31 +0530

    Update

--- BINARY SEARCH/Aggressive_Cows.cpp
+++ BINARY SEARCH/Aggressive_Cows.cpp
@@ -1,36 +1,41 @@
-class Solution { // TC = O(nlogn)
-    public:
-  // Binary search apporoach - exactly opposite of Book allocation problem 
-      int aggressiveCows(vector<int> &stalls, int k) {
-          int st = 1 , end , mid, ans;
-          int n = stalls.size();
-          // sort arr in increasing order
-          sort(stalls.begin(),stalls.end());
-      end = stalls[n-1] - stalls[0];
-      // apply binary search on this range
-      while(st <= end) // O(Logn)
-      {// least allowed min distance b/w cows can be greater but not less
-          mid = st + (end - st)/2;
-          int count = 1, pos = stalls[0]; // assigned 1st cow to 1st stall
-          for(int i = 1; i < n ; i++)// O(n)
-          {
-              if(pos + mid <= stalls[i])
-              { // if we are able to assign cows keeping mindist then increase cows until req k 
-                  count ++;
-                  pos = stalls[i];
-              }
-          }
-      if(count < k) // < no of cows        
-          end = mid - 1; // unable to assign cows keeping minallowed dist then Increase the min allowed dist
-      else 
-      {
-          ans = mid; // if able to assign // store in ans 
-          //then go right to find max possible minallowed dist
-          st = mid + 1;
-      }
-      }
-      return ans;
-      }
-      
-  };
-  
+#include <bits/stdc++.h>
+using namespace std;
+
+// TC = O(N * LogN) + O(N LogD)
+// SC = O(1)
+
+class Solution {
+  public:
+    int aggressiveCows(vector<int> &stalls, int k) {
+        sort(stalls.begin(), stalls.end());
+        
+        auto canPlace = [&](int dist) {
+            int cows = 1, last = stalls[0];
+            
+            for(int i = 1; i < stalls.size(); i++) {
+                if(stalls[i] - last >= dist) {
+                    cows++;
+                    last = stalls[i];
+                }
+            }
+            
+            return cows >= k;
+        };
+        
+        int lo = 1, hi = stalls.back() - stalls.front();
+        int ans = 0;
+        
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if(canPlace(mid)) {
+                ans = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+};
