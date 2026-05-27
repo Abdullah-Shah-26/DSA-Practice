479ee58a3a4fba93dbff44016c6add2ecbba525c
Abdullah Shah <syedmohammedabdullahshah@gmail.com>
2026-05-28 02:46:38 +0530

    Update

---
+++ ARRAYS/4_Sum_2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+class Solution {
+public:
+  int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+  unordered_map<ll, int> mp;
+
+  for(int a : nums1){
+    for(int b : nums2){
+      mp[a + b]++;
+    }
+  }
+
+  int cnt = 0; 
+  for(int c : nums3){
+    for(int d : nums4){
+      if(mp.count(-(c + d))){
+        cnt += mp[-(c + d)];
+      }
+    }
+  }
+
+  return cnt; 
+  }
+};
