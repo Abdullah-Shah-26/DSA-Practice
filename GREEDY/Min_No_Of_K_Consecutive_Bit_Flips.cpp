eb6f4840c82768ff14c9ed2af3deffca87cbcccf
Abdullah Shah <syedmohammedabdullahshah@gmail.com>
2026-06-24 16:07:19 +0530

    Update

--- GREEDY/Min_No_Of_K_Consecutive_Bit_Flips.cpp
+++ GREEDY/Min_No_Of_K_Consecutive_Bit_Flips.cpp
@@ -8,19 +8,18 @@ public:
   int minKBitFlips(vector<int>& nums, int k) {
   int n = nums.size();
   int ans = 0, times = 0;
-  
-  vector<bool> flip(n, 0);
+  vector<bool> flip(n, false);
 
   for(int i = 0; i < n; i++){
-    if(i >= k)
+    if(i >= k) // Removing effect of flip that doesnt apply to current window [i-k+1, i]
       times -= flip[i - k];
 
     if((nums[i] == 1 && (times & 1)) || (nums[i] == 0 && (times % 2 == 0))){
-      if(i + k > n)
+      if(i + k > n) // Cannot fix last (k - 1) bits even if they need, since we can dont have k bits to flip
         return -1;
 
-      ans++;
       times++;
+      ans++;
       flip[i] = 1;
     }
   }    
