d52c56172c85ff4c30e0171b2d1c0054225412ec
Abdullah Shah <syedmohammedabdullahshah@gmail.com>
2026-07-02 21:20:10 +0530

    Update

--- RECURSION/Combination_Sum.cpp
+++ RECURSION/Combination_Sum.cpp
@@ -1,41 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{ //*Worst-Case Time Complexity: O(3^n)
- 
-public: // tar == combiantionSUM 
- 
+class Solution {
+public:
+  set<vector<int>> st;
 
-//!creating a set to track unique combinations 
-set<vector<int>>s;
+  void dfs(vector<int> &a, int i, int tar, vector<int> &cur,
+           vector<vector<int>> &ans) {
 
-void getAllCombinations(vector<int> &arr, int idx, int tar, vector<vector<int>> &ans, vector<int> &combin){
-  if(idx == arr.size() || tar<0){//base case targetsum not found after travelling whole arr or tar cant be -ve
-    return;
-  }
-  if(tar == 0){ // means our commbination equals targetsum
-    if(s.find(combin) == s.end()){ //!checks whether combination already exists in set
-    ans.push_back(combin); //store ans 
-      s.insert(combin); //!  insert that combination in set
+    if (tar == 0) {
+      if (!st.count(cur)) {
+        st.insert(cur);
+        ans.push_back(cur);
+      }
+      return;
     }
-    return;
+
+    if (i == a.size() || tar < 0)
+      return;
+
+    // Take once
+    cur.push_back(a[i]);
+    dfs(a, i + 1, tar - a[i], cur, ans);
+
+    // Take again
+    dfs(a, i, tar - a[i], cur, ans);
+    cur.pop_back();
+
+    // Skip
+    dfs(a, i + 1, tar, cur, ans);
   }
 
-  combin.push_back(arr[idx]);     
-  // single time element inclusion choice
-  getAllCombinations(arr, idx+1, tar - arr[idx], ans, combin);
-  // multiple inclusion choice
-  getAllCombinations(arr, idx, tar - arr[idx], ans, combin);
-  combin.pop_back(); // backtracking step popping the included element before returning to prev calls 
-  //exclusion of element ki choice
-  getAllCombinations(arr, idx+1 , tar - arr[idx], ans, combin);
-}
+  vector<vector<int>> combinationSum(vector<int> &a, int tar) {
+    vector<vector<int>> ans;
+    vector<int> cur;
 
-vector<vector<int>> combinationSum(vector<int> &arr, int target){
-  vector<vector<int>> ans;
-  vector<int> combin;
-
-  getAllCombinations(arr, 0, target, ans, combin);
-  return ans;
-}
+    dfs(a, 0, tar, cur, ans);
+    return ans;
+  }
 };
