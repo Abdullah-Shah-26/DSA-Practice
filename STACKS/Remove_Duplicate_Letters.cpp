59679b080c34bf448d0554734b219a89e6002ed7
Abdullah Shah <syedmohammedabdullahshah@gmail.com>
2026-08-05 23:03:17 +0530

    Update

---
+++ STACKS/Remove_Duplicate_Letters.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  string removeDuplicateLetters(string s) {
+    int n = s.size();
+
+    string ans = "";
+
+    vector<bool> taken(26, false);
+    vector<int> lastSeen(26);
+
+    for(int i = 0; i < n; i++)
+      lastSeen[s[i] - 'a'] = i;
+
+    for(int i = 0; i < n; i++){
+      int idx = s[i] - 'a';
+
+      if(taken[idx])
+        continue;
+
+      while(ans.size() > 0 && ans.back() > s[i] && lastSeen[ans.back() - 'a'] > i){
+        taken[ans.back() - 'a'] = false;
+        ans.pop_back();
+      } 
+
+      taken[s[i] - 'a'] = true;
+      ans += s[i];
+    } 
+
+    return ans;   
+  }
+};
