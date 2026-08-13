03d8c3025b08f8b41f9d43935f226d5ae61b2066
Abdullah Shah <syedmohammedabdullahshah@gmail.com>
2026-08-13 17:55:13 +0530

    Update

---
+++ STRINGS/Shortest_Palindrome.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  string shortestPalindrome(string s) {
+    string rev = s;
+    reverse(begin(rev), end(rev));
+
+    for (int i = 0; i < s.size(); i++) {
+
+      if (!memcmp(s.c_str(), rev.c_str() + i, s.size() - i)) {
+        return rev.substr(0, i) + s;
+      }
+    }
+    return rev + s;
+  }
+};
+
